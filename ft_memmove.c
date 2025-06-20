f26e3c715ae8280ad42e0753f5e3eda85e983b87
Mertcan Cetinkaya <mecetink@k2m09s03.42kocaeli.com.tr>
2025-06-20 15:37:03 +0300

    explicit tc

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mecetink <mecetink@42student.kocaeli.co    +#+  +:+       +#+        */
+/*   By: mecetink <mecetink@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 15:16:33 by mecetink          #+#    #+#             */
-/*   Updated: 2025/06/05 01:24:37 by mecetink         ###   ########.fr       */
+/*   Updated: 2025/06/20 15:36:48 by mecetink         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	{
 		while (i < n)
 		{
-			((char *)dest)[i] = ((char *)src)[i];
+			((char *) dest)[i] = ((char *) src)[i];
 			i++;
 		}
 	}
 	else
 	{
 		while (n--)
-			((char *)dest)[n] = ((char *)src)[n];
+			((char *) dest)[n] = ((char *) src)[n];
 	}
 	return (dest);
 }
